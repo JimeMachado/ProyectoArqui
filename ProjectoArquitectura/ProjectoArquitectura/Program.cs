01b6062c445e20ee5a9b8f2b83ffb0b26c731d4d
Jime <jimemr09@gmail.com>
2016-10-01 21:14:17 +0000

    prueba puch con inicializacion de variables

--- ProjectoArquitectura/ProjectoArquitectura/Program.cs
+++ ProjectoArquitectura/ProjectoArquitectura/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +12,25 @@ namespace ProyectoArqui
         private int[] memoria_datos;
         private int[] memoria_instruciones;
 
+        private int[] bus_datos;
+        private int[] bud_intruciones;
+
         private int[,] cache_datos_nucleo1;
         private int[,] cache_datos_nucleo2;
         private int[,] cache_datos_nucleo3;
+
+        private int[,] cache_instruciones_nucleo1;
+        private int[,] cache_instruciones_nucleo2;
+        private int[,] cache_instruciones_nucleo3;
+
+        private int PC1;
+        private int PC2;
+        private int PC3;
+      
+        private long reloj;
+
+        private bool fin_programa;
+
 
 
 
