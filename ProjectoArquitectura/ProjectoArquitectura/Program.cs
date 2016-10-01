7479e0c8af5e94ffb10f8f66b66b0bd6596d04cf
Jime <jimemr09@gmail.com>
2016-10-01 21:03:50 +0000

    primer commit

---
+++ ProjectoArquitectura/ProjectoArquitectura/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoArqui
+{
+    class Procesador
+    {
+
+        private int[] memoria_datos;
+        private int[] memoria_instruciones;
+
+        private int[,] cache_datos_nucleo1;
+        private int[,] cache_datos_nucleo2;
+        private int[,] cache_datos_nucleo3;
+
+
+
+
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+
+   
+}
