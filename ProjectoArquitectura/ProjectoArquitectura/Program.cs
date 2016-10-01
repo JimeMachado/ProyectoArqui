3e66f4e9aa7ef8294ae4c885f76a86e4fa6b62c3
Scott Adams <scottadamsgarrido@hotmail.com>
2016-10-01 16:05:40 -0600

    poop

--- ProjectoArquitectura/ProjectoArquitectura/Program.cs
+++ ProjectoArquitectura/ProjectoArquitectura/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,14 +26,10 @@ namespace ProyectoArqui
         private int PC1;
         private int PC2;
         private int PC3;
-      
+
         private long reloj;
 
         private bool fin_programa;
-
-
-
-
 
         static void Main(string[] args)
         {
@@ -42,5 +38,138 @@ namespace ProyectoArqui
         }
     }
 
-   
+    class instruccionesP1
+    {
+
+    }
+
+    class instruccionesP2
+    {
+
+    }
+
+    class instrucciones
+    {
+        private void instruccionesP1(int[] inst, int hilillo)
+        {
+            switch (inst[0])
+            {
+                case 8:
+                    break;
+
+                case 32:
+                    break;
+
+                case 34:
+                    break;
+
+                case 12:
+                    break;
+
+                case 14:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 3:
+                    break;
+                case 2:
+                    break;
+
+                case 35:
+                    break;
+
+                case 43:
+                    break;
+
+                case 63:
+                    break;
+            }
+        }
+
+        private void instruccionesP2(int[] inst, int hilillo)
+        {
+            switch (inst[0])
+            {
+                case 8:
+                    break;
+
+                case 32:
+                    break;
+
+                case 34:
+                    break;
+
+                case 12:
+                    break;
+
+                case 14:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 3:
+                    break;
+                case 2:
+                    break;
+
+                case 35:
+                    break;
+
+                case 43:
+                    break;
+
+                case 63:
+                    break;
+            }
+        }
+        private void instruccionesP3(int[] inst, int hilillo)
+        {
+            switch (inst[0])
+            {
+                case 8:
+                    break;
+
+                case 32:
+                    break;
+
+                case 34:
+                    break;
+
+                case 12:
+                    break;
+
+                case 14:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 3:
+                    break;
+                case 2:
+                    break;
+
+                case 35:
+                    break;
+
+                case 43:
+                    break;
+
+                case 63:
+                    break;
+            }
+        }
+    }
 }
